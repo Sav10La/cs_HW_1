d9191970cf4d3ab201bdf0378f927bc07fb92a4f
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-06-24 20:37:51 +0500

    add Task004 EvenInRange

---
+++ Task004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите диапазон, начиная с 1 ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Все четные числа с 1 до {number}:");
+
+int i = 2;
+
+while (i <= number)
+{
+    Console.Write($"{i} ");
+    i +=2;
+}
